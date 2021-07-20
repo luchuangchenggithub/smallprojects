658bf89aee787662757ef03faeffad207153fea4
lu_chuang_cheng <luchuangcheng@zuzuche.com>
2021-07-20 17:29:47 +0800

    0720-1

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 # -*-coding:utf-8 -*-
 
 
-print([i for i in range(1,11) if i%2==0])
-a = [1,2,3,4,5]
-print([i**2 for i in a])
+print(tuple([i for i in range(1, 101) if i % 3 == 1]))
---
+++ 元组字典相关/字典的value转为str.py
@@ -0,0 +1,6 @@
+# -*-coding:utf-8 -*-
+
+a = {'a':11,'b':22}
+for i,j in a.items():
+    a[i]=str(j)
+print(a)
